c209974dc5aa0e9b36e35598e79076362186ef81
GOPIKA126 <gopikagopan126@gmail.com>
2024-07-03 13:22:01 +0530

    Update streamlit_app.py
    dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,10 +49,11 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         
-        st.success('Your Smoothie is ordered132!', icon="✅")
+        st.success('Your Smoothie is ordered!', icon="✅")
     st.write(my_insert_stmt)
     st.stop()
 
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+#st.text(fruityvice_response.json())
+ fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=true)
